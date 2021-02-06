a5687f965b0cf8ada98f3d55b46ec23607792d19
tianchengcheng <tcheng8866@126.com>
2021-02-06 10:53:53 +0800

    多余的路由

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,9 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Element Admin',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      // title 没有注册到vuex数据上
+      title: 'Vue Admin',
+      logo: require('@/assets/va-logo.png')
     }
   }
 }
